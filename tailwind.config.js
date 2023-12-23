b263ab611d35170d3344c594ae4461f97992eb31
Rysth <johnpalacios.t@gmail.com>
2023-12-22 19:16:59 -0500

    Remove MaterialTailwind and Add Preline.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/preline/preline.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
